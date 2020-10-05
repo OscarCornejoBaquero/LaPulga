f28fdedf5fda40dead68b06b0eb9f96b9317cdf3
oscar <oscar@DESKTOP-MCGGIET>
2020-10-04 21:01:34 -0500

    Creacion de Contructores y Get y Set Clientes

--- src/Objetos/Clientes.java
+++ src/Objetos/Clientes.java
@@ -8,5 +8,65 @@ public class Clientes {
     private String telefono;
     private String domicilio;
     private String correo;
+
+    public Clientes() {
+    }
+
+    public Clientes(String cedula, String nombre, String apellido, String telefono, String domicilio, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.domicilio = domicilio;
+        this.correo = correo;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
     
 }
